825c72365dbbb2c33e2f50baa08d58967d1d3e7a
Javad <81755531+JNemune@users.noreply.github.com>
2023-07-23 23:19:11 +0330

    Update BinaryToASCII.py to python3

--- BinaryToASCII.py
+++ BinaryToASCII.py
@@ -1,50 +1,48 @@
 # -*- encoding: utf-8 -*-
 import struct
 
-infile = open('binary.stl') #import file
-out = open('ASCII.stl', 'w') #export file
+infile = open("binary.stl", "rb")  # import file
+out = open("ASCII.stl", "w")  # export file
 
 data = infile.read()
 
 
 out.write("solid ")
 
-for x in xrange(0,80):
-    if not ord(data[x]) == 0:
-        out.write(struct.unpack('c', data[x])[0])
-    else:
-        pass
+for x in range(0, 80):
+    if not data[x] == 0:
+        out.write(chr(data[x]))
 out.write("\n")
 
-number = data[80] + data[81] + data[82] + data[83] 
-faces = struct.unpack('I',number)[0]
+number = data[80:84]
+faces = struct.unpack("I", number)[0]
 
-for x in range(0,faces):
+for x in range(0, faces):
     out.write("facet normal ")
 
-    xc = data[84+x*50] + data[85+x*50] + data[86+x*50] + data[87+x*50]
-    yc = data[88+x*50] + data[89+x*50] + data[90+x*50] + data[91+x*50]
-    zc = data[92+x*50] + data[93+x*50] + data[94+x*50] + data[95+x*50]
+    xc = data[84 + x * 50 : 88 + x * 50]
+    yc = data[88 + x * 50 : 92 + x * 50]
+    zc = data[92 + x * 50 : 96 + x * 50]
 
-    out.write(str(struct.unpack('f',xc)[0]) + " ")
-    out.write(str(struct.unpack('f',yc)[0]) + " ")
-    out.write(str(struct.unpack('f',zc)[0]) + "\n")
+    out.write(str(struct.unpack("f", xc)[0]) + " ")
+    out.write(str(struct.unpack("f", yc)[0]) + " ")
+    out.write(str(struct.unpack("f", zc)[0]) + "\n")
 
     out.write("outer loop\n")
 
-    for y in range(1,4):
+    for y in range(1, 4):
         out.write("vertex ")
 
-        xc = data[84+y*12+x*50] + data[85+y*12+x*50] + data[86+y*12+x*50] + data[87+y*12+x*50]
-        yc = data[88+y*12+x*50] + data[89+y*12+x*50] + data[90+y*12+x*50] + data[91+y*12+x*50]
-        zc = data[92+y*12+x*50] + data[93+y*12+x*50] + data[94+y*12+x*50] + data[95+y*12+x*50]
+        xc = data[84 + y * 12 + x * 50 : 88 + y * 12 + x * 50]
+        yc = data[88 + y * 12 + x * 50 : 92 + y * 12 + x * 50]
+        zc = data[92 + y * 12 + x * 50 : 96 + y * 12 + x * 50]
 
-        out.write(str(struct.unpack('f',xc)[0]) + " ")
-        out.write(str(struct.unpack('f',yc)[0]) + " ")
-        out.write(str(struct.unpack('f',zc)[0]) + "\n")
+        out.write(str(struct.unpack("f", xc)[0]) + " ")
+        out.write(str(struct.unpack("f", yc)[0]) + " ")
+        out.write(str(struct.unpack("f", zc)[0]) + "\n")
 
     out.write("endloop\n")
     out.write("endfacet\n")
 
 out.close()
-print "end"
+print("end")
